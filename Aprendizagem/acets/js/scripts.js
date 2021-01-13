f2a8faca04e78b7b006a7c01b4312ac2a4850839
Lucas <Lucas@D9997NE038.edu.fb.org.br>
2021-01-13 15:03:51 -0300

    Início projeto captação de cliente

---
+++ Aprendizagem/acets/js/scripts.js
@@ -0,0 +1,13 @@
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+
+function validaNome(){
+    let divNome = docutmento.querySelector("#divnome")
+    if(nome.value.length < 3){
+        
+    }
+}
